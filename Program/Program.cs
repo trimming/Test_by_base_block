5b4cc95b9ef97769386518804655846af9604e29
Alexey Golikov <lexgolikoff@gmail.com>
2023-07-01 22:48:29 +0300

    some update from README.md & Program.cs

---
+++ Program/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите группы символов через запятую: ");
+string collection = Console.ReadLine()!;
+string[] array = collection.Split(", ");
+
+
+string[] GetSortArray(string[] array)
+{
+    string sort = String.Empty;
+    foreach (var item in array)
+    {
+        if (item.Length <= 3)
+        {
+            sort += item + " ";
+        }
+    }
+    string[] sortArray = sort.TrimEnd().Split(" ");
+    return sortArray;
+}
+
+Console.WriteLine($"[{String.Join(", ", GetSortArray(array))}]");
